8ee3b2803b5d2788abea5eb130d576ff2997c53d
Ito Takumi <e125743@ie.u-ryukyu.ac.jp>
2016-12-08 07:07:28 +0900

    CythonOpenmpTest

---
+++ cython-setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+
+ext_module = Extension(
+    "testMulti",
+    ["testMulti.pyx"],
+    extra_compile_args=['-fopenmp'],
+    extra_link_args=['-fopenmp'],
+)
+
+setup(
+    name = 'Hello world app',
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = cythonize([ext_module]),
+)
--- testMulti.py
+++
@@ -1,9 +0,0 @@
-from cython.parallel import parallel, threadid
-import sys
-
-list = [1,2,3,4,5,6,7,8,9,10]
-
-for i in list:
-  with parallel(int(sys.argv[1])):
-    print(threadid())
-    print(i)
